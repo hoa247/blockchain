dc519b397e5cded6e461ee7ddeb283031be09302
hoa247 <trandinhhoa.tc@gmail.com>
2021-12-09 17:44:28 +0700

    add comment

--- calculate_nonce.js
+++ calculate_nonce.js
@@ -1,3 +1,12 @@
+/*
+-block: 3
+-nonce: 1
+-data:
+    + trans1: +6$
+    + trans2: -5$
+- preHash: 000097f1ad678374cbec40bda3f96dec36dcf863b4913ad876a08449051d8332
+*/
+
 const sha256 = require('crypto-js/sha256');
 
 let block = {
@@ -8,7 +17,6 @@ let block = {
         trans2: '-5$',
     },
     preHash: '000097f1ad678374cbec40bda3f96dec36dcf863b4913ad876a08449051d8332',
-
 }
 let hash = '';
 while (hash.substring(0, 4) !== '0000') {
