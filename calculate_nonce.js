cfe27926d9b1a11ab378824255a4e4e25636ce53
hoa247 <trandinhhoa.tc@gmail.com>
2021-12-09 17:40:13 +0700

    first commit

---
+++ calculate_nonce.js
@@ -0,0 +1,21 @@
+const sha256 = require('crypto-js/sha256');
+
+
+let block = {
+    block: 3,
+    nonce: 1,
+    data: {
+        trans1: '+6$',
+        trans2: '-5$',
+    },
+    preHash: '000097f1ad678374cbec40bda3f96dec36dcf863b4913ad876a08449051d8332',
+
+}
+let hash = '';
+while (hash.substring(0, 4) !== '0000') {
+    block.nonce++;
+    hash = sha256(JSON.stringify(block)).toString()
+    console.log(hash);
+}
+block.hash = hash
+console.log(block);
